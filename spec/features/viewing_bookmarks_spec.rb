f7b75306767df41fb50d488a03d9b2b2d37051c2
Ralph Mallett <rmallett12@gmail.com>
2020-10-15 11:35:35 +0100

    Feat: add spec helper to truncate test database before tests

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,6 +1,12 @@
 feature 'viewing bookmarks' do
   scenario 'bookmarks page shows all bookmarks' do
+
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+    connection.exec("INSERT INTO bookmarks (url) VALUES('https://www.nfl.com/uk/');")
+    connection.exec("INSERT INTO bookmarks (url) VALUES('https://www.goal.com/en-gb');")
+
     visit '/bookmarks'
+
     expect(page).to have_content 'https://www.goal.com/en-gb'
     expect(page).to have_content 'https://www.nfl.com/uk/'
   end
