89930acba38580f5f2348f0946add73dd79e277b
Ralph Mallett <rmallett12@gmail.com>
2020-10-15 11:15:02 +0100

    Test: create test database and update tests

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -3,7 +3,12 @@ require 'bookmark'
 describe Bookmark do
   describe '.all' do
     it 'returns all bookmarks' do
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+      connection.exec("INSERT INTO bookmarks (url) VALUES('https://www.nfl.com/uk/');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES('https://www.goal.com/en-gb');")
+
       bookmarks = Bookmark.all
+
       expect(bookmarks).to include('https://www.nfl.com/uk/')
       expect(bookmarks).to include('https://www.goal.com/en-gb')
     end
