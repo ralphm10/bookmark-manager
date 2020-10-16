850c7da470f4f5f756402c47c31ebe89b41f7058
Ralph Mallett <rmallett12@gmail.com>
2020-10-16 12:18:06 +0100

    Feat: bookmark displays title and link to url

--- app.rb
+++ app.rb
@@ -8,6 +8,7 @@ class BookmarkManager < Sinatra::Base
 
   get '/bookmarks' do
     @bookmarks = Bookmark.all
+    p @bookmarks
     erb :'bookmarks/index'
   end
 
--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -8,7 +8,7 @@ class Bookmark
       connection = PG.connect(dbname: 'bookmark_manager')
     end
     result = connection.exec('SELECT * FROM bookmarks')
-    result.map { |bookmark| bookmark['url'] }
+    # result.map { |bookmark| bookmark['title'] }
   end
 
   def self.create(url:, title:)
