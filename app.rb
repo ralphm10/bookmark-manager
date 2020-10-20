556bc25cc3b76f4951cd781237c3f39314762d01
Ralph Mallett <rmallett12@gmail.com>
2020-10-20 11:27:56 +0100

    Refactor: add database connection methods to bookmark methods

--- app.rb
+++ app.rb
@@ -3,7 +3,6 @@ require './lib/bookmark'
 require './database_connection_setup'
 
 class BookmarkManager < Sinatra::Base
-    
   enable :sessions, :method_override
 
   get '/' do
@@ -34,11 +33,11 @@ class BookmarkManager < Sinatra::Base
     @bookmark = Bookmark.find(id: params[:id])
     erb :'bookmarks/edit'
   end
-  
+
   patch '/bookmarks/:id' do
     Bookmark.update(id: params[:id], url: params[:url], title: params[:title])
     redirect('/bookmarks')
-  end 
-  
-  run! if app_file == $0
+  end
+
+  run! if app_file == $PROGRAM_NAME
 end
--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,57 +1,32 @@
 require 'pg'
+require 'database_connection'
 
 class Bookmark
-
   def self.all
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'bookmark_manager_test')
-    else
-      connection = PG.connect(dbname: 'bookmark_manager')
-    end
-    result = connection.exec('SELECT * FROM bookmarks')
+    result = DatabaseConnection.query('SELECT * FROM bookmarks')
     result.map do |bookmark|
       Bookmark.new(id: bookmark['id'], title: bookmark['title'], url: bookmark['url'])
     end
   end
 
   def self.create(url:, title:)
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'bookmark_manager_test')
-    else
-      connection = PG.connect(dbname: 'bookmark_manager')
-    end
-    result = connection.exec("INSERT INTO bookmarks (url, title) VALUES('#{url}', '#{title}') RETURNING id, url, title;")
+    result = DatabaseConnection.query("INSERT INTO bookmarks (url, title) VALUES('#{url}', '#{title}') RETURNING id, url, title;")
     Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
   end
 
   def self.delete(id:)
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'bookmark_manager_test')
-    else
-      connection = PG.connect(dbname: 'bookmark_manager')
-    end
-    connection.exec("DELETE FROM bookmarks WHERE id=#{id};")
+    DatabaseConnection.query("DELETE FROM bookmarks WHERE id=#{id};")
   end
 
   def self.update(id:, url:, title:)
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'bookmark_manager_test')
-    else
-      connection = PG.connect(dbname: 'bookmark_manager')
-    end
-    result = connection.exec("UPDATE bookmarks SET url='#{url}', title ='#{title}' WHERE id =#{id} RETURNING id, url, title;")
+    result = DatabaseConnection.query("UPDATE bookmarks SET url='#{url}', title ='#{title}' WHERE id =#{id} RETURNING id, url, title;")
     Bookmark.new(id: result[0]['id'], url: result[0]['url'], title: result[0]['title'])
   end
 
   def self.find(id:)
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'bookmark_manager_test')
-    else
-      connection = PG.connect(dbname: 'bookmark_manager')
-    end
-    result = connection.exec("SELECT * FROM bookmarks WHERE id = '#{id}';")
+    result = DatabaseConnection.query("SELECT * FROM bookmarks WHERE id = '#{id}';")
     Bookmark.new(id: result[0]['id'], url: result[0]['url'], title: result[0]['title'])
-  end 
+  end
 
   attr_reader :id, :title, :url
 
