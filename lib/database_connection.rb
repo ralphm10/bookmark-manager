80adf86a83670321e995a6d5c94946022da63a5f
Ralph Mallett <rmallett12@gmail.com>
2020-10-20 10:50:26 +0100

    Test: add test for persistent db connection

--- lib/database_connection.rb
+++ lib/database_connection.rb
@@ -1,6 +1,14 @@
 class DatabaseConnection
 
   def self.setup(db)
-    @connection = PG.connect(dbname: "#{db}")
+    @connection = PG.connect(dbname: db)
+  end 
+
+  def self.connection
+    @connection
+  end 
+
+  def self.query(query_string)
+    @connection.query(query_string)
   end 
 end 
--- spec/database_connection_spec.rb
+++ spec/database_connection_spec.rb
@@ -6,5 +6,18 @@ describe DatabaseConnection do
       connection = DatabaseConnection.setup('bookmark_manager_test')
       expect(connection.port).to eq 5432
     end
+    it 'is a persistent connection' do
+      connection = DatabaseConnection.setup('bookmark_manager_test')
+      expect(DatabaseConnection.connection).to eq connection
+    end
+  end
+
+  describe './query' do
+    it 'makes an SQL query on a database' do
+      connection = DatabaseConnection.setup('bookmark_manager_test')
+      first_bookmark = Bookmark.create(url: 'https://www.hotels.com/', title: 'Hotels')
+      query = DatabaseConnection.query("SELECT * FROM bookmarks WHERE title = 'Hotels'")
+      expect(query.first['url']).to eq 'https://www.hotels.com/'
+    end
   end
 end
